1df4aa7cffe845d0b2e1054143a79637d84764fd
Tiago Rodrigues de Almeida <tiago.almeida@oru.se>
2023-01-23 20:28:14 +0100

    prepare synchronized data

--- magni_dash/run_dash.py
+++ magni_dash/run_dash.py
@@ -4,24 +4,29 @@ from magni_dash.visualization.trajectories import load_data, play_data
 from magni_dash.config.constants import TRAJECTORY_SAMPLES_PATH
 
 
-files = os.listdir(TRAJECTORY_SAMPLES_PATH)
-files_target = list(filter(lambda x: x.endswith("pp.tsv"), files))
-
 st.title("Magni Dashboard")
-option = st.selectbox(
-    label="File", options=files_target, key="input_file", label_visibility="visible"
-)
-if st.session_state.input_file:
-    load_data()
-    min_frame, max_frame = int(st.session_state["df"].index.min()), int(
-        st.session_state["df"].index.max()
+modes = ["raw data", "synchronized data"]
+page = st.radio("Mode", modes)
+
+if page == "raw data":
+    files = os.listdir(TRAJECTORY_SAMPLES_PATH)
+    files_target = list(filter(lambda x: x.endswith("pp.tsv"), files))
+    option = st.selectbox(
+        label="File", options=files_target, key="input_file", label_visibility="visible"
     )
-    frame = st.slider(
-        label="Frame",
-        min_value=min_frame,
-        max_value=max_frame,
-        value=min_frame,
-        step=1,
-        key="current_frame",
-    )
-    play_data()
+    if st.session_state.input_file:
+        load_data()
+        min_frame, max_frame = int(st.session_state["df"].index.min()), int(
+            st.session_state["df"].index.max()
+        )
+        frame = st.slider(
+            label="Frame",
+            min_value=min_frame,
+            max_value=max_frame,
+            value=min_frame,
+            step=1,
+            key="current_frame",
+        )
+        play_data()
+elif page == "synchronized data":
+    st.write("TODO")
