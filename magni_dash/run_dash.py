db415743d17088c0ee09bd7721783714ccbce07c
Tiago Rodrigues de Almeida <tiago.almeida@oru.se>
2023-01-23 20:17:25 +0100

    from pyplot to plotly working with slidebar

--- magni_dash/run_dash.py
+++ magni_dash/run_dash.py
@@ -1,17 +1,27 @@
 import os
 import streamlit as st
-from magni_dash.visualization.trajectories import show_file_traj_data
+from magni_dash.visualization.trajectories import load_data, play_data
 from magni_dash.config.constants import TRAJECTORY_SAMPLES_PATH
 
 
 files = os.listdir(TRAJECTORY_SAMPLES_PATH)
 files_target = list(filter(lambda x: x.endswith("pp.tsv"), files))
 
-
 st.title("Magni Dashboard")
 option = st.selectbox(
     label="File", options=files_target, key="input_file", label_visibility="visible"
 )
-
 if st.session_state.input_file:
-    show_file_traj_data()
+    load_data()
+    min_frame, max_frame = int(st.session_state["df"].index.min()), int(
+        st.session_state["df"].index.max()
+    )
+    frame = st.slider(
+        label="Frame",
+        min_value=min_frame,
+        max_value=max_frame,
+        value=min_frame,
+        step=1,
+        key="current_frame",
+    )
+    play_data()
--- magni_dash/visualization/trajectories.py
+++ magni_dash/visualization/trajectories.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+import plotly.express as px
 import streamlit as st
 import os
 import re
@@ -9,33 +9,37 @@ from magni_dash.data_preprocessing.trajectory_data import process_data
 from magni_dash.config.constants import TRAJECTORY_SAMPLES_PATH
 
 
-def show_file_traj_data():
-    data_load_state = st.text("Loading data...")
-    file_name = st.session_state["input_file"]
+def load_data():
     raw_df = load_df(
-        df_path=os.path.join(TRAJECTORY_SAMPLES_PATH, file_name),
+        df_path=os.path.join(TRAJECTORY_SAMPLES_PATH, st.session_state["input_file"]),
         sep="\t",
         header=11,
         index_col="Frame",
     )
     processed_df = process_data(raw_df=raw_df)
+    st.session_state["df"] = processed_df
 
-    data_load_state.text("Loading data...done!")
 
+def play_data():
+    processed_df = st.session_state["df"]
     regex_pattern = re.compile(r"H\d+")
-    helmet_number = regex_pattern.findall(file_name)[0][1:]
-    st.write(
-        processed_df[
-            [f"Helmet_{helmet_number} - 1 X", f"Helmet_{helmet_number} - 1 Y"]
-        ].head(5)
+    helmet_number = regex_pattern.findall(st.session_state["input_file"])[0][1:]
+    fig = px.line(
+        processed_df[: st.session_state["current_frame"]],
+        x=f"Helmet_{helmet_number} - 1 X",
+        y=f"Helmet_{helmet_number} - 1 Y",
+        title=f"Data from Helmet {helmet_number}",
     )
-
-    fig = plt.figure(figsize=(20, 10))
-    plt.scatter(
-        processed_df[f"Helmet_{helmet_number} - 1 X"],
-        processed_df[f"Helmet_{helmet_number} - 1 Y"],
-        s=1,
+    fig.update_layout(
+        xaxis=dict(showgrid=False),
+        yaxis=dict(showgrid=False),
+        xaxis_range=[
+            processed_df[f"Helmet_{helmet_number} - 1 X"].min(),
+            processed_df[f"Helmet_{helmet_number} - 1 X"].max(),
+        ],
+        yaxis_range=[
+            processed_df[f"Helmet_{helmet_number} - 1 Y"].min(),
+            processed_df[f"Helmet_{helmet_number} - 1 Y"].max(),
+        ],
     )
-    plt.title(f"Data from Helmet {helmet_number}", fontsize=40)
-    plt.axis("off")
-    st.pyplot(fig)
+    st.plotly_chart(fig, use_container_width=True)
