56575c511e0c1026a61890e7f32aff5a452e60b7
tmralmeida <tmr.almeida96@gmail.com>
2023-05-18 15:25:31 +0200

    server path updated

--- magni_dash/config/constants.py
+++ magni_dash/config/constants.py
@@ -1,4 +1,4 @@
 TRAJECTORY_SAMPLES_PATH = (
-    "/home/tmr/Documents/PhD/My_PhD/Magni/magni-dash-samples/trajectories"
+    "/home/toaa/magni-dash-samples/trajectories"
 )
 TRAJECTORY_DATA_TYPE = "2D"
